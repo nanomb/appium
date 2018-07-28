f609d995df8c884043fd48039430a00ce710cf36
Fernando Martin <nanomb@gmail.com>
2018-07-28 07:59:03 +0200

    Scenario skeleton steps

---
+++ appium/src/test/java/com/capgemini/test/appium/stepdefinitions/LoginSteps.java
@@ -0,0 +1,34 @@
+package com.capgemini.test.appium.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps {
+
+
+@Given("^we are a user$")
+public void we_are_a_user() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Given("^we enter to application$")
+public void we_enter_to_application() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^we make login with user and password$")
+public void we_make_login_with_user_and_password() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^the login is successfull$")
+public void the_login_is_successfull() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+}
